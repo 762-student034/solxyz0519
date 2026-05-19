d97280287e41c748b98fd9fdeb770ac67904828c
PC80184-PC <PC80184-PC@PC80184>
2026-05-19 15:47:38 +0900

    aa

--- src/solxyz0519/p01/Test01.java
+++ src/solxyz0519/p01/Test01.java
@@ -1,9 +1,10 @@
 package solxyz0519.p01;
 
 public class Test01 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hell world!");
+		System.out.println("Hello world!");
 	}
 
 }
