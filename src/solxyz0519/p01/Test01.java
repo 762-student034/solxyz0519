ec269254e7fbbb0baa2d91a5df0f20f528f6405e
762-student034 <762-student034@la-study.com>
2026-05-19 16:05:20 +0900

    Daimon

--- src/solxyz0519/p01/Test01.java
+++ src/solxyz0519/p01/Test01.java
@@ -3,10 +3,11 @@ package solxyz0519.p01;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hell world!");
+		System.out.println("Hello world!");
 		System.out.println("Hello world!");
 		System.out.println("Hello tamachi!");
 		System.out.println("Hello mita");
+		System.out.println("Hello daimon");
 	}
 
 }
