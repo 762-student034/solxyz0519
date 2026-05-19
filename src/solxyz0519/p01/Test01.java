57b4bb3edee45d73aa4e158a1cd894455535f393
PC80181-PC <PC80181-PC@PC80181>
2026-05-19 16:02:59 +0900

    sol 2nd

--- src/solxyz0519/p01/Test01.java
+++ src/solxyz0519/p01/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hell world!");
 		System.out.println("Hello world!");
+		System.out.println("Hello tamachi!");
 	}
 
 }
